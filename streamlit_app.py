7a12569f46d00f6f3e2d9f71351dc3f02ec5f2fa
snowflake-train <112783843+snowflake-train@users.noreply.github.com>
2022-09-03 15:41:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,9 @@ streamlit.text('Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
 
 streamlit.header('Build your own Breakfast Smoothie')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
