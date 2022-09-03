aa5218a3b175b39f254b7a90c4a2e85f58788195
snowflake-train <112783843+snowflake-train@users.noreply.github.com>
2022-09-03 15:37:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 
 import streamlit
 
-streamlit.title('Hello World !')
+streamlit.title('My Parents Healthy Diner !')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+
+streamlit.text('Spinach and Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free Range Egg')
