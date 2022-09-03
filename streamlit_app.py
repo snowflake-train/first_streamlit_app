7a7a72338bdef43f2c5b4278bfd480376a0ef685
snowflake-train <112783843+snowflake-train@users.noreply.github.com>
2022-09-03 15:42:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,17 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Diner !')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-
 streamlit.text('Spinach and Rocket Smoothie')
-
 streamlit.text('Hard-Boiled Free Range Egg')
-
 streamlit.header('Build your own Breakfast Smoothie')
 
 import pandas
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
+
