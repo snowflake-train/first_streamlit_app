bce709bf26a9b60c5b3d25c14b41facc9ab1c7be
snowflake-train <112783843+snowflake-train@users.noreply.github.com>
2022-09-04 13:35:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -31,7 +30,6 @@ try:
   if not fruit_choice:
     stremlit.error("Please select a fruit to get information")
  else:   
-  #streamlit.write('The user entered ', fruit_choice)
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   streamlit.dataframe(fruityvice_normalized)
