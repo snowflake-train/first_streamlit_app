b90ff793f868783f54395c27fba21f973e29b670
snowflake-train <112783843+snowflake-train@users.noreply.github.com>
2022-09-04 13:33:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ try:
   if not fruit_choice:
     stremlit.error("Please select a fruit to get information")
  else:   
-  streamlit.write('The user entered ', fruit_choice)
+  #streamlit.write('The user entered ', fruit_choice)
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   streamlit.dataframe(fruityvice_normalized)
